547e05ca8b60b93802ce21e286b28011bee0eb5c
Raymond Hampson <rayhampson4@gmail.com>
2026-04-09 14:28:43 -0700

    Add Products DbSet to ApplicationDbContext
    Enable EF Core to manage Product entities by adding a DbSet<Product> property to ApplicationDbContext.

--- InventoryTracker/Data/ApplicationDbContext.cs
+++ InventoryTracker/Data/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using InventoryTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryTracker.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+
+        /// <summary>
+        /// This allows the DbContext to manage the Product table in the database, 
+        /// which shows all products that belong to a Manufacturer.
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
     }
 }
