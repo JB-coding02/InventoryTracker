0fcb05fb7c945cd220d346852ca4d21253436409
Raymond Hampson <rayhampson4@gmail.com>
2026-04-14 19:34:03 -0700

    Update ProductController to fetch product by id asynchronously
    Refactored ProductController to use dependency injection for ApplicationDbContext. Modified Index action to accept an id parameter.

--- InventoryTracker/Controllers/ProductController.cs
+++ InventoryTracker/Controllers/ProductController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InventoryTracker.Data;
+using InventoryTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryTracker.Controllers;
 
 public class ProductController : Controller
 {
-	public IActionResult Index ()
+	private readonly ApplicationDbContext _context;
+
+	public ProductController (ApplicationDbContext context)
 	{
-		return View();
+		_context = context;
 	}
+
+	public async Task<IActionResult> Index (int id)
+	{
+		Product? product = await _context.Products
+			.Where(p => p.ProductId == id)
+			.FirstOrDefaultAsync();
+
+		return View(product);
+	}
+
+
 }
