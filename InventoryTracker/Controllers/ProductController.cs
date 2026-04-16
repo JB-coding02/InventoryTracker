55c7a110db753031830c7d9734a7cc22d455a01a
Raymond Hampson <rayhampson4@gmail.com>
2026-04-16 14:15:30 -0700

    Add product page (#24)

---
+++ InventoryTracker/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using InventoryTracker.Data;
+using InventoryTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryTracker.Controllers;
+
+public class ProductController : Controller
+{
+	private readonly ApplicationDbContext _context;
+
+	public ProductController (ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+
+	/// <summary>
+	/// Displays the details view for the specified product.
+	/// </summary>
+	/// <param name="id">The unique identifier of the product to display.</param>
+	/// <returns>The task result contains an <see cref="IActionResult"/> that
+	/// renders the product details view if the product is found; otherwise, a NotFound result.</returns>
+	public async Task<IActionResult> Index (int id)
+	{
+		Product? product = await _context.Products
+			.Include(p => p.Manufacturer) // Eager load the related Manufacturer data
+			.FirstOrDefaultAsync(p => p.ProductId == id);
+
+		if (product == null) { 
+			return NotFound();
+		}
+		return View(product);
+	}
+
+
+}
